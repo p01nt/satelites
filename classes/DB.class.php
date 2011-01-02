3b7e86dca16437c80bf4129ff3e5dc4a39d06eb0
Pasha Klets <pasha@klets.name>
2011-01-02 16:15:01 +0200

    database class

---
+++ classes/DB.class.php
@@ -0,0 +1,78 @@
+<?php
+class DB {
+	private $query = array();
+	private $fields = array();
+	private $link = null;
+	static private $master = array('host' => null, 'user' => null, 'password' => null);
+	static private $database;
+
+	private function __construct() {
+		$this->__connect();
+	}
+
+	static public function get() {
+		return new self();
+	}
+
+	private function __connect() {
+		if (!empty(self::$database)) {
+			$this->link = mysql_connect(self::$master['host'], self::$master['user'], self::$master['password']);
+			$result = mysql_select_db(self::$database, $this->link);
+			if (!$result) {
+				// @TODO exeption
+			}
+		}
+	}
+
+	public function set_master_host($host, $user, $password) {
+		self::$master['host'] = $host;
+		self::$master['user'] = $user;
+		self::$master['password'] = $password;
+	}
+
+	public function set_database($database) {
+		self::$database = $database;
+	}
+
+	public function query() {
+		$query = implode(' ', $this->query);
+		$result = mysql_query($query, $this->link);
+		
+		if (!$result)
+		{
+			//@TODO nice exeption 
+			dd(mysql_error());
+		}
+
+		return $result;
+	}
+
+	public function select()
+	{
+		$this->query[] = 'select';
+		return $this;
+	}
+
+	public function from($table) {
+		$this->table = $table;
+		return 	$this;
+	}
+
+	public function fetch() {
+		if (empty($this->fields)) {
+			$this->query[] = '*';
+		}
+
+		$this->query[] = 'from `' . $this->table . '`';
+
+		$result = $this->query();
+
+		$rows = array();
+		while ($row = mysql_fetch_object($result)) {
+			$rows[] = $row;
+		}
+
+		return $rows;
+	}
+}
+?>
