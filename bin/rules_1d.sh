6b5a28a4f256be142ef08c18d27211ce6be94319
Pasha Klets <pasha@klets.name>
2011-01-10 20:19:12 +0200

    cron

--- bin/rules_1d.sh
+++ bin/rules_1d.sh
@@ -1,5 +1,5 @@
 home='/server/www/satelites'
-php $home/bin/updateWhois.php
+php $home/bin/updateWhois.php > /dev/null
 php $home/public/index.php update > /dev/null
 php $home/public/index.php > /dev/null
 
